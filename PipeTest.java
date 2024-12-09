b5045aaac6ded9f02795e9d220af6d55eff56c41
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2024-12-08 21:26:27 -0300

    Add files via upload

---
+++ PipeTest.java
@@ -0,0 +1,35 @@
+/**
+ * @version 1.20 1999-04-23
+ * @author Cay Horstmann
+ */
+package PipeTest;
+
+import java.util.*;
+import java.io.*;
+
+public class PipeTest
+{  public static void main(String args[])
+   {  try
+      {  /* set up pipes */
+         PipedOutputStream pout1 = new PipedOutputStream();
+         PipedInputStream pin1 = new PipedInputStream(pout1);
+
+         PipedOutputStream pout2 = new PipedOutputStream();
+         PipedInputStream pin2 = new PipedInputStream(pout2);
+
+         /* construct threads */
+
+         Producer prod = new Producer(pout1);
+         Filter filt = new Filter(pin1, pout2);
+         Consumer cons = new Consumer(pin2);
+
+         /* start threads */
+
+         prod.start();
+         filt.start();
+         cons.start();
+      }
+      catch (IOException e){}
+   }
+}
+
